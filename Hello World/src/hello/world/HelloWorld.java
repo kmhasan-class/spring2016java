e19be522f3fab0364281be81e09f1b3229dfbf29
kmhasan <kmhasan@SEUPC0657>
2016-01-26 10:31:57 +0600

    Hello World project

--- Hello World/src/hello/world/HelloWorld.java
+++ Hello World/src/hello/world/HelloWorld.java
@@ -16,6 +16,7 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         System.out.printf("Hello World\n");
+        System.out.println("Here goes another line\n");
     }
     
 }
