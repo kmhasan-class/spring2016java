ebe0b050420a1e5fdab3753dd783e6f41047e0ef
kmhasan <kmhasan@SEUPC0657>
2016-01-26 10:46:50 +0600

    Console Arithmetic

---
+++ Console Arithmetic/src/console/arithmetic/ConsoleArithmetic.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package console.arithmetic;
+
+/**
+ *
+ * @author kmhasan
+ */
+public class ConsoleArithmetic {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+        int c = a + b;
+        System.out.printf("Result %d\n", c);
+    }
+    
+}
