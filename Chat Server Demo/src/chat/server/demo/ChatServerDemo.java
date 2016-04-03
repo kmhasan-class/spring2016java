c0f47879ea6d40a33aa81605d848f25151b953b0
kmhasan <kmhasan@SEUPC0657>
2016-04-03 10:37:32 +0600

    Chat Server

---
+++ Chat Server Demo/src/chat/server/demo/ChatServerDemo.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chat.server.demo;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author kmhasan
+ */
+public class ChatServerDemo {
+
+    public ChatServerDemo() {
+        int portNumber = 9876;
+        try {
+            // Socket - a medium for communication
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+            System.out.printf("Server running on port %d\n", portNumber);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.printf("Connected with %s\n", socket.getInetAddress());
+                InputStream in = socket.getInputStream();
+                byte[] message = new byte[1000];
+                int length = in.read(message);
+                System.out.printf("Client sent: [%s]\n", new String(message).substring(0, length));
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(ChatServerDemo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new ChatServerDemo();
+    }
+
+}
